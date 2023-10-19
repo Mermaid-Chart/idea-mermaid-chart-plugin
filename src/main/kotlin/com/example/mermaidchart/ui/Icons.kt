5a57890e482f2327d21c0f3aacb854510512c2e9
Daniil Kurchanov <dkurch84@gmail.com>
2023-10-19 12:34:58 +0300

    Add gutter icon

---
+++ src/main/kotlin/com/example/mermaidchart/ui/Icons.kt
@@ -0,0 +1,8 @@
+package com.example.mermaidchart.ui
+
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.util.IconLoader
+
+object Icons {
+    val Mermaid = IconLoader.getIcon("/icons/mermaid-icon.svg", Icons::class.java)
+}
--- src/main/kotlin/com/example/mermaidchart/ui/editorHighlight/MermaidHighlightVisitor.kt
+++ src/main/kotlin/com/example/mermaidchart/ui/editorHighlight/MermaidHighlightVisitor.kt
@@ -1,26 +1,49 @@
 package com.example.mermaidchart.ui.editorHighlight
 
+import com.example.mermaidchart.ui.Icons
 import com.example.mermaidchart.utils.isMermaidLink
 import com.example.mermaidchart.utils.mermaidLinkRange
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.editor.markup.EffectType
+import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.openapi.editor.markup.TextAttributes
-import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import java.awt.Color
 import java.awt.Font
+import javax.swing.Icon
 
 class MermaidCommentAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
         if (!element.isMermaidLink()) return
 
         val linkRange = element.mermaidLinkRange() ?: return
-        //Skip "//" for highlight
         holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
             .range(linkRange)
             .enforcedTextAttributes(TextAttributes(null, Color(255, 71, 123, 77), null, EffectType.BOXED, Font.PLAIN))
+            .gutterIconRenderer(SideIconGutterRenderer(linkRange.startOffset))
             .create()
     }
+}
+
+private class SideIconGutterRenderer(private val rangeStart: Int): GutterIconRenderer() {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is SideIconGutterRenderer) return false
+
+        if (rangeStart != other.rangeStart) return false
+
+        return true
+    }
+
+    override fun getAlignment(): Alignment = Alignment.RIGHT
+
+    override fun isDumbAware(): Boolean = true
+    override fun hashCode(): Int {
+        return rangeStart
+    }
+
+    override fun getIcon(): Icon = Icons.Mermaid
+
 }
