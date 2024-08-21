696396664b9b17ab4779f8d13b4128cc0e8c3652
Daniil Kurchanov <dkurch84@gmail.com>
2024-08-21 12:20:13 +0300

    Update Gradle plugin to 2.0 Update untilBuild to 242 Update Gradle Wrapper to 8.5 Fix minor code problems

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,26 @@
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+import org.jetbrains.intellij.platform.gradle.models.ProductRelease
+
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.0"
     kotlin("plugin.serialization") version "1.9.10"
-    id("org.jetbrains.intellij") version "1.15.0"
+    id("org.jetbrains.intellij.platform") version "2.0.1"
 }
-
-group = "co.tula.mermaidchart"
-version = "1.1.6"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
+    intellijPlatform {
+        create("2024.2")
+        instrumentationTools()
+        pluginVerifier()
+    }
     implementation(libs.bundles.ktor) {
         exclude(group = "org.jetbrains.kotlin")
         exclude(group = "org.jetbrains.kotlinx")
@@ -21,12 +29,44 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2023.3")
+val paramSinceBuild = "223"
+val paramUntilBuild = "242.*"
 
-    plugins.set(listOf())
+intellijPlatform {
+    version = "1.1.7"
+    group = "co.tula.mermaidchart"
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = paramSinceBuild
+            untilBuild = paramUntilBuild
+        }
+    }
+
+    pluginVerification {
+        ides {
+            select {
+                types = listOf(IntelliJPlatformType.IntellijIdeaCommunity)
+                channels = listOf(ProductRelease.Channel.RELEASE)
+                sinceBuild = paramSinceBuild
+                /*
+                    https://github.com/JetBrains/intellij-platform-gradle-plugin/issues/1719
+                    Testing against 242.* is impossible at the moment
+                    TODO: Update "241.*" to paramUntilBuild once issue will be fixed
+                 */
+                untilBuild = "241.*"
+            }
+        }
+    }
+
+    signing {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishing {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
 }
 
 tasks {
@@ -37,20 +77,5 @@ tasks {
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
-    }
-
-    patchPluginXml {
-        sinceBuild.set("223")
-        untilBuild.set("241.*")
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
     }
 }
--- src/main/kotlin/co/tula/mermaidchart/data/serializers/OffsetDateTimeSerializer.kt
+++ src/main/kotlin/co/tula/mermaidchart/data/serializers/OffsetDateTimeSerializer.kt
@@ -1,5 +1,6 @@
 package co.tula.mermaidchart.data.serializers
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
 import kotlinx.serialization.encoding.Decoder
@@ -7,6 +8,7 @@ import kotlinx.serialization.encoding.Encoder
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 
+@OptIn(ExperimentalSerializationApi::class)
 @Serializer(forClass = OffsetDateTime::class)
 object OffsetDateTimeSerializer: KSerializer<OffsetDateTime> {
     private val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
